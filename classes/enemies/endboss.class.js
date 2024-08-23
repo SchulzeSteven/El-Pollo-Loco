e7e268fd98ca9d0cdbfb8b7926a98c5666c9c3dd
Steven Schulze <schulze89@hotmail.de>
2024-08-23 16:36:23 +0200

    added endboss dead

--- classes/enemies/endboss.class.js
+++ classes/enemies/endboss.class.js
@@ -57,6 +57,7 @@ class Endboss extends MoveableObject {
         this.loadImages(this.IMAGES_WALKING);
         this.loadImages(this.IMAGES_ATTACK);
         this.loadImages(this.IMAGES_HURT);
+        this.loadImages(this.IMAGES_DEAD);
         this.x = 3050;
         this.y = 55;
         this.hitsToKill = 5;
@@ -100,18 +101,65 @@ class Endboss extends MoveableObject {
     takeHit() {
         if (this.isHurt || !this.hasStartedMoving) 
             return; // Treffer ignorieren, wenn der Endboss bereits in Hurt-Animation ist oder noch nicht läuft
-        this.life -= 20;  // Reduziert die Lebenspunkte um 20 pro Treffer
-        this.isHurt = true;  // Setze Hurt-Zustand
-        this.playAnimation(this.IMAGES_HURT, 60);  // Spiele die Hurt-Animation langsamer ab (250ms pro Frame)
     
-        setTimeout(() => {
-            this.isHurt = false;  // Setze Hurt-Zustand zurück, um erneut getroffen werden zu können
-        }, this.IMAGES_HURT.length * 250);  // Länge der Hurt-Animation: 3 Bilder * 250ms
+        this.life -= 20;  // Reduziert die Lebenspunkte um 20 pro Treffer
+    
+        if (this.life <= 19 && this.life > 0) {
+            this.isHurt = false;  // Setze Hurt-Zustand zurück, um zu verhindern, dass die Hurt-Animation angezeigt wird
+            this.stopWalking();  // Deaktiviert das Gehen
+            this.playDeadAnimation();  // Spiele die langsame Todesanimation ab
+        } else if (this.life >= 20) {
+            this.isHurt = true;  // Setze Hurt-Zustand
+            this.stopWalking();  // Deaktiviert das Gehen
+            this.playHurtAnimation();  // Spiele die Hurt-Animation ab
+    
+            setTimeout(() => {
+                this.isHurt = false;  // Setze Hurt-Zustand zurück, um erneut getroffen werden zu können
+                this.startWalking();  // Reaktiviert das Gehen nach der Hurt-Animation
+            }, this.IMAGES_HURT.length * 150);  // Länge der Hurt-Animation: 3 Bilder * 150ms
+        }
     
         this.world.statusBar.updateEndbossLife(this.life);
     
         if (this.life <= 0) {
-            this.removeEndboss();
+            this.stopWalking();  // Deaktiviert das Gehen
+            this.playDeadAnimation();  // Langsame Todesanimation abspielen, wenn das Leben auf 0 fällt
+            // removeEndboss wird nun in der playDeadAnimation aufgerufen
+        }
+    }
+    
+    playHurtAnimation() {
+        let currentImageIndex = 0;
+        const hurtInterval = setInterval(() => {
+            if (this.isHurt && currentImageIndex < this.IMAGES_HURT.length) {
+                this.loadImage(this.IMAGES_HURT[currentImageIndex]);
+                currentImageIndex++;
+            } else {
+                clearInterval(hurtInterval);
+            }
+        }, 150);  // Verlangsamt die Animation auf 150ms pro Frame
+    }
+    
+    playDeadAnimation() {
+        let currentImageIndex = 0;
+        const interval = setInterval(() => {
+            if (currentImageIndex < this.IMAGES_DEAD.length) {
+                this.loadImage(this.IMAGES_DEAD[currentImageIndex]);
+                currentImageIndex++;
+            } else {
+                clearInterval(interval);
+                this.removeEndboss();  // Entfernt den Endboss, nachdem die Animation abgeschlossen ist
+            }
+        }, 200);  // Verlangsamt die Animation auf 200ms pro Frame
+    }
+
+    stopWalking() {
+        this.isWalking = false;  // Setzt den Walking-Zustand auf false
+    }
+
+    startWalking() {
+        if (this.hasStartedMoving && !this.isHurt) {
+            this.isWalking = true;  // Reaktiviert das Gehen, wenn die Hurt-Animation beendet ist
         }
     }
 
